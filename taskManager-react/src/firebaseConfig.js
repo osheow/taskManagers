3fd701632356d04d90c7e131a60b9c72e8c1cd33
Eric Hwang <erichwangfolder@Erics-MacBook-Pro.local>
2022-11-16 12:58:18 -0800

    updated firebase

--- taskManager-react/src/firebaseConfig.js
+++ taskManager-react/src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAs4e6y2jZonC1AcFFLG931zgVRqqgb8OE",
-  authDomain: "basic-blog-173f5.firebaseapp.com",
-  projectId: "basic-blog-173f5",
-  storageBucket: "basic-blog-173f5.appspot.com",
-  messagingSenderId: "310512611673",
-  appId: "1:310512611673:web:be338b5fc633a0d0e65c48",
+  apiKey: "AIzaSyD2LZ4Dm6UXrVYIoG_ODMv36L90LwZ_wNs",
+  authDomain: "taskmanagers-e7d31.firebaseapp.com",
+  projectId: "taskmanagers-e7d31",
+  storageBucket: "taskmanagers-e7d31.appspot.com",
+  messagingSenderId: "454439418534",
+  appId: "1:454439418534:web:d2d1ff3fdb5878c7f4d267"
 };
 
 export const app = initializeApp(firebaseConfig);
