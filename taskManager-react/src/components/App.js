c8c93b5e3ff3953f268f6809c1acbe2223d5a8b1
Troy Wu <ywu30@lion.lmu.edu>
2022-11-30 12:42:51 -0800

    update

--- taskManager-react/src/components/App.js
+++ taskManager-react/src/components/App.js
@@ -18,26 +18,6 @@ export default function App() {
   const [writing, setWriting] = useState(false);
   const user = useAuthentication();
 
-  // This is a trivial app, so just fetch all the articles only when
-  // a user logs in. A real app would do pagination. Note that
-  // "fetchArticles" is what gets the articles from the service and
-  // then "setArticles" writes them into the React state.
-  useEffect(() => {
-    if (user) {
-      fetchArticles().then(setArticles);
-    }
-  }, [user]);
-
-  // Update the "database" *then* update the internal React state. These
-  // two steps are definitely necessary.
-  function addArticle({ title, body }) {
-    createArticle({ title, body }).then((article) => {
-      setArticle(article);
-      setArticles([article, ...articles]);
-      setWriting(false);
-    });
-  }
-
   return (
     <div className="App">
       <header>
@@ -45,16 +25,6 @@ export default function App() {
         {user && <button onClick={() => setWriting(true)}>New Event</button>}
         {!user ? <SignIn /> : <SignOut />}
       </header>
-
-      {/*{!user ? "" : <Nav articles={articles} setArticle={setArticle} />}
-
-      {!user ? (
-        ""
-      ) : writing ? (
-        <ArticleEntry addArticle={addArticle} />
-      ) : (
-        <Article article={article} />
-      )}*/}
       <div id="container-left">
         <h3>Recommended Watchlist</h3>
       </div>
