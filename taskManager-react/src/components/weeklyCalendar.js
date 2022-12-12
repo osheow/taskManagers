37fe1382a9a021b8108d4e9104dd3af7a4477f4d
Eric Hwang <erichwangfolder@Erics-MBP.home>
2022-12-11 20:58:29 -0800

    final version

--- taskManager-react/src/components/weeklyCalendar.js
+++ taskManager-react/src/components/weeklyCalendar.js
@@ -73,40 +73,11 @@ class WCalendar extends Component {
     //make a collection in firestore called events, and write these events into firestore
     //blog can be helpful for this
 
-    const events = [
-      {
-        id: 1,
-        text: "Event 1",
-        start: "2023-03-07T08:30:00",
-        end: "2023-03-07T13:00:00",
-        backColor: "#B8F2E6",
-      },
-      {
-        id: 2,
-        text: "Event 2",
-        start: "2023-03-08T09:30:00",
-        end: "2023-03-08T11:30:00",
-        backColor: "#6aa84f",
-      },
-      {
-        id: 3,
-        text: "Event 3",
-        start: "2023-03-08T12:00:00",
-        end: "2023-03-08T15:00:00",
-        backColor: "#f1c232",
-      },
-      {
-        id: 4,
-        text: "Event 4",
-        start: "2023-03-06T11:30:00",
-        end: "2023-03-06T14:30:00",
-        backColor: "#cc4125",
-      },
-    ];
+    
 
     const startDate = "2023-03-07";
 
-    this.calendar.update({ startDate, events });
+    this.calendar.update({ startDate});
   }
 
   render() {
