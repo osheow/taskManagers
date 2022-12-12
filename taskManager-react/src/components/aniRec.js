131eb980a85190545e2ee215f8b10a6ee88e9bb1
Eric Hwang <erichwangfolder@Erics-MBP.home>
2022-12-11 18:56:29 -0800

    trying to add anime

---
+++ taskManager-react/src/components/aniRec.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const Anirec = () => {
+  const [anime, setAnime] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.jikan.moe/v4/random/anime')
+    .then(response => response.json())
+    .then(data => setAnime(data));
+  }, []);
+
+return(
+    <div>
+    <h4>{anime.title}</h4>
+    <img src={anime.image_url} alt={anime.title} />
+    </div>
+);
+};
+
+export default Anirec;
